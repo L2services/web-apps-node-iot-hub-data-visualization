100f1bae42d23e5e0fc43bebb7ed9a59a7b7a141
Joshua Lyon <josh@boshdirect.com>
2017-05-26 15:59:14 -0500

    Modified for NodeMCU data from Grove ADXL345 and Temperature Sensor

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,8 +1,20 @@
 $(document).ready(function () {
+  //setup our data arrays
   var timeData = [],
     temperatureData = [],
-    humidityData = [];
-  var data = {
+    humidityData = [],
+    //acceleration
+    axData = [],
+    ayData = [],
+    azData = [],
+    //orientation?
+    xData = [],
+    yData = [],
+    zData = [];
+
+
+// --------- temperature chart -------------
+  var temperatureChartData = {
     labels: timeData,
     datasets: [
       {
@@ -10,30 +22,16 @@ $(document).ready(function () {
         label: 'Temperature',
         yAxisID: 'Temperature',
         borderColor: "rgba(255, 204, 0, 1)",
-        pointBoarderColor: "rgba(255, 204, 0, 1)",
         backgroundColor: "rgba(255, 204, 0, 0.4)",
-        pointHoverBackgroundColor: "rgba(255, 204, 0, 1)",
-        pointHoverBorderColor: "rgba(255, 204, 0, 1)",
         data: temperatureData
-      },
-      {
-        fill: false,
-        label: 'Humidity',
-        yAxisID: 'Humidity',
-        borderColor: "rgba(24, 120, 240, 1)",
-        pointBoarderColor: "rgba(24, 120, 240, 1)",
-        backgroundColor: "rgba(24, 120, 240, 0.4)",
-        pointHoverBackgroundColor: "rgba(24, 120, 240, 1)",
-        pointHoverBorderColor: "rgba(24, 120, 240, 1)",
-        data: humidityData
       }
     ]
   }
 
-  var basicOption = {
+  var temperatureChartBasicOption = {
     title: {
       display: true,
-      text: 'Temperature & Humidity Real-time Data',
+      text: 'Temperature Real-time Data',
       fontSize: 36
     },
     scales: {
@@ -41,32 +39,150 @@ $(document).ready(function () {
         id: 'Temperature',
         type: 'linear',
         scaleLabel: {
-          labelString: 'Temperature(C)',
+          labelString: 'Temperature (C)',
           display: true
         },
         position: 'left',
-      }, {
-          id: 'Humidity',
-          type: 'linear',
-          scaleLabel: {
-            labelString: 'Humidity(%)',
-            display: true
-          },
-          position: 'right'
-        }]
+      }]
     }
   }
 
   //Get the context of the canvas element we want to select
-  var ctx = document.getElementById("myChart").getContext("2d");
+  var ctxTemperature = document.getElementById("temperatureChart").getContext("2d");
   var optionsNoAnimation = { animation: false }
-  var myLineChart = new Chart(ctx, {
+  var myTemperatureLineChart = new Chart(ctxTemperature, {
     type: 'line',
-    data: data,
-    options: basicOption
+    data: temperatureChartData,
+    options: temperatureChartBasicOption
   });
 
-  var ws = new WebSocket('wss://' + location.host);
+  // //------- second chart for acceleration ----------
+  // var accelerationChartData = {
+  //   labels: timeData,
+  //   datasets: [
+  //     {
+  //       fill: false,
+  //       label: 'X',
+  //       yAxisID: 'acceleration',
+  //       borderColor: "rgba(255, 0, 0, 1)",
+  //       backgroundColor: "rgba(255, 0, 0, 0.4)",
+  //       data: axData
+  //     },
+  //     {
+  //       fill: false,
+  //       label: 'Y',
+  //       yAxisID: 'acceleration',
+  //       borderColor: "rgba(0, 255, 0, 1)",
+  //       backgroundColor: "rgba(0, 255, 0, 0.4)",
+  //       data: ayData
+  //     },
+  //     {
+  //       fill: false,
+  //       label: 'Z',
+  //       yAxisID: 'acceleration',
+  //       borderColor: "rgba(0, 0, 255, 1)",
+  //       backgroundColor: "rgba(0, 0, 255, 0.4)",
+  //       data: azData
+  //     }
+  //   ]
+  // }
+
+  // var accelerationChartBasicOption = {
+  //   title: {
+  //     display: true,
+  //     text: 'Acceleration Real-time Data',
+  //     fontSize: 36
+  //   },
+  //   scales: {
+  //     yAxes: [{
+  //       id: 'acceleration',
+  //       type: 'linear',
+  //       scaleLabel: {
+  //         labelString: 'Acceleration',
+  //         display: true
+  //       },
+  //       position: 'left',
+  //     }]
+  //   }
+  // }
+
+  // //Get the context of the canvas element we want to select
+  // var ctxAcceleration = document.getElementById("accelerationChart").getContext("2d");
+  // var myAccelerationLineChart = new Chart(ctxAcceleration, {
+  //   type: 'line',
+  //   data: accelerationChartData,
+  //   options: accelerationChartBasicOption
+  // });
+
+
+
+  //------- third chart for Orientation ----------
+  var orientationChartData = {
+    labels: timeData,
+    datasets: [
+      {
+        fill: false,
+        label: 'X',
+        yAxisID: 'XYZ',
+        borderColor: "rgba(255, 0, 0, 1)",
+        backgroundColor: "rgba(255, 0, 0, 0.4)",
+        data: xData
+      },
+      {
+        fill: false,
+        label: 'Y',
+        yAxisID: 'XYZ',
+        borderColor: "rgba(0, 255, 0, 1)",
+        backgroundColor: "rgba(0, 255, 0, 0.4)",
+        data: yData
+      },
+      {
+        fill: false,
+        label: 'Z',
+        yAxisID: 'XYZ',
+        borderColor: "rgba(0, 0, 255, 1)",
+        backgroundColor: "rgba(0, 0, 255, 0.4)",
+        data: zData
+      }
+    ]
+  }
+
+  var orientationChartBasicOption = {
+    title: {
+      display: true,
+      text: 'Orientation Real-time Data',
+      fontSize: 36
+    },
+    scales: {
+      yAxes: [{
+        id: 'XYZ',
+        type: 'linear',
+        scaleLabel: {
+          labelString: 'Orientation',
+          display: true
+        },
+        position: 'left',
+      }]
+    }
+  }
+
+  //Get the context of the canvas element we want to select
+  var ctxOrientation = document.getElementById("orientationChart").getContext("2d");
+  var myOrientationLineChart = new Chart(ctxOrientation, {
+    type: 'line',
+    data: orientationChartData,
+    options: orientationChartBasicOption
+  });
+
+  // ------ Setup Websockets and data connectivity
+
+  var wsprotocol;
+  if (location.protocol === "https:") {
+    wsprotocol = "wss://";
+  } else {
+    wsprotocol = "ws://"; //for local testing
+  }
+  var ws = new WebSocket(wsprotocol + location.host);
   ws.onopen = function () {
     console.log('Successfully connect WebSocket');
   }
@@ -74,21 +190,31 @@ $(document).ready(function () {
     console.log('receive message' + message.data);
     try {
       var obj = JSON.parse(message.data);
-      if(!obj.time || !obj.temperature || !obj.humidity) {
+      if(!obj.time || !obj.Temperature) {
         return;
       }
       timeData.push(obj.time);
-      temperatureData.push(obj.temperature);
-      humidityData.push(obj.humidity);
+      temperatureData.push(obj.Temperature);
+      //acceleration
+      axData.push(obj.AcceX);
+      ayData.push(obj.AcceY);
+      azData.push(obj.AcceZ);
+      //orientation?
+      xData.push(obj.X);
+      yData.push(obj.Y);
+      zData.push(obj.Z);
+      //humidityData.push(obj.humidity);
 
       // only keep no more than 50 points in the line chart
       var len = timeData.length;
       if (len > 50) {
         timeData.shift();
         temperatureData.shift();
-        humidityData.shift();
+        //humidityData.shift();
       }
-      myLineChart.update();
+      //myAccelerationLineChart.update();
+      myTemperatureLineChart.update();
+      myOrientationLineChart.update();
     } catch (err) {
       console.error(err);
     }
